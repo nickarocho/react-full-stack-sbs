a28048e2b0b91121f90b94fd940ed3d4a096e0c1
Nick Arocho <nick@boldlyfine.com>
2018-02-22 12:33:28 -0800

    initialize react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import App from './components/App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(<Router><App /></Router>, document.getElementById('root'));
